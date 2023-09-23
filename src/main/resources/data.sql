ee97af1c3d970519a73ce19a9101e3c07757be1e
zhangcg <cg.zhang001@163.com>
2023-09-23 23:34:47 +0900

    [initial] init practice

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,3 +18,17 @@ VALUES
 ( '著作者索条件', NULL, '三浦健太郎', NULL, false),
 ( '著作者索条件', NULL, NULL, '2018-11-30', false)
 ;
+
+INSERT INTO customer
+(cname, founded_on, capital, address, telno, note)
+VALUES
+('株式会社NTTデータ', '2022/11/1', 142520000000, '東京都江東区豊洲3-3-3 豊洲センタービル', '03-5546-8202', 'メイン取引'),
+('富士通株式会社', '1935/6/20', 325638181205, '東京都港区東新橋1-5-2 汐留シティセンター', '044-777-1111', 'メイン取引')
+;
+
+INSERT INTO bill
+(bname, bill_limit_date, bill_acount, bill_to, bill_info, transfer_bank, transfer_name, transfer_account, note)
+VALUES
+('Nttdata様への9月SES案件Aの請求書', '2023/10/15', 9000000, '株式会社NTTデータ', '9月SES案件Aの請求書', 'みずほ銀行', '株式会社ピカソフト', '12345678', 'メイン口座に振込請求'),
+('富士通様への9月SES案件Bの請求書', '2023/10/15', 9900000, '富士通株式会社', '9月SES案件Bの請求書', '三井住友銀行', '株式会社ピカソフト', '87654321', 'サブ口座に振込請求')
+;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,3 +25,36 @@ published_date_condition DATE,
 delete_flag boolean,
 PRIMARY KEY(id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--customer検索条件があれば削除
+DROP TABLE IF EXISTS customer;
+
+--customerがなければ新しく作成
+CREATE TABLE IF NOT EXISTS customer (
+    cid int AUTO_INCREMENT, 
+    cname varchar(300) NOT NULL, 
+    founded_on date , 
+    capital numeric(20) DEFAULT 0, 
+    address varchar(500), 
+    telno varchar(100), 
+    note varchar(1000), 
+    PRIMARY KEY (cid)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--bill検索条件があれば削除
+DROP TABLE IF EXISTS bill;
+
+--billがなければ新しく作成
+CREATE TABLE IF NOT EXISTS bill (
+    bid int AUTO_INCREMENT, 
+    bname varchar(300) NOT NULL, 
+    bill_limit_date date , 
+    bill_acount numeric(10) DEFAULT 0, 
+    bill_to varchar(300), 
+    bill_info varchar(100), 
+    transfer_bank varchar(300), 
+    transfer_name varchar(300), 
+    transfer_account varchar(8), 
+    note varchar(1000), 
+    PRIMARY KEY (bid)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
